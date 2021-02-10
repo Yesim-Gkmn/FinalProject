4a8faa8a46b189b6881fff80dc9982410b8e2243
Yeşim Gökmen <yesimgokmen24@gmail.com>
2021-02-11 00:05:29 +0300

    10.02.2021

---
+++ Core/Utilities/Results/SuccesDataResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccesDataResul<T>:DataResult<T>
+    {
+        public SuccesDataResul(T data ,string message):base(data,true,message)
+        {
+
+        }
+        public SuccesDataResul(T data):base(data,true)
+        {
+
+        }
+        public SuccesDataResul(string message):base(default,true,message)
+        {
+
+        }
+        public SuccesDataResul():base(default,true)
+        {
+
+        }
+
+    }
+}
